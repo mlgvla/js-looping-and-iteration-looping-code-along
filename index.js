3238d1a260bccccb1bf65c3fed654eadad607f72
mlgvla <mlgerardvla@gmail.com>
2021-07-06 22:30:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// Code your solutions in this file
-// comment
+function writeCards(names, event) {
+    let thankYouMessages = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)   
+    }
+    return thankYouMessages
+}
+
+function countDown(number) {
+   while (number >= 0) {
+       console.log(number)
+       number--
+   }
+}
